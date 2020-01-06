349442fbc71909aa2d71fc1003cb8a61b3a79908
贠乾 <yunqian8@live.com>
2020-01-06 17:01:41 +0800

    1，新增IdentityServer4测试类型 2，新增appsetting.js配置 操作类 3，开启IdentityServer4验证

--- Blog.IdentityServer4/Startup.cs
+++ Blog.IdentityServer4/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +13,45 @@ namespace Blog.IdentityServer4
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+            // in-memory, code config
+            .AddTestUsers(InMemoryConfig.Users().ToList())
+            .AddInMemoryApiResources(InMemoryConfig.GetApiResources())
+            .AddInMemoryClients(InMemoryConfig.GetClients());
+
+
+            builder.AddDeveloperSigningCredential();
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+
+            services.AddAuthentication();//配置认证服务
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,17 +61,22 @@ namespace Blog.IdentityServer4
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseIdentityServer()
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
