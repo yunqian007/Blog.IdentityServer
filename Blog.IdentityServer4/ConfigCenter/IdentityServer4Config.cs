5f35ac657608877e60d84cc84af86b5bcf8c9354
贠乾 <yunqian8@live.com>
2020-01-08 14:45:26 +0800

    重新定义配置类 IdentityServer4Config

---
+++ Blog.IdentityServer4/ConfigCenter/IdentityServer4Config.cs
@@ -0,0 +1,60 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.IdentityServer4.ConfigCenter
+{
+    public class IdentityServer4Config
+    {
+        // scopes define the resources in your system
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource("roles", "角色", new List<string> { JwtClaimTypes.Role }),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+                new ApiResource("blog.core.api", "Blog.Core API") {
+                    // include the following using claims in access token (in addition to subject id)
+                    //requires using using IdentityModel;
+                    UserClaims = { JwtClaimTypes.Name, JwtClaimTypes.Role }
+                }
+            };
+        }
+
+        // clients want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            // javascript client
+            return new List<Client> {
+                new Client {
+                    ClientId = "blogvuejs",
+                    ClientName = "Blog.Vue JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris =           { "http://localhost:6688/callback" },
+                    PostLogoutRedirectUris = { "http://localhost:6688" },
+                    AllowedCorsOrigins =     { "http://localhost:6688" },
+
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "roles",
+                        "blog.core.api"
+                    }
+                }
+            };
+        }
+    }
+}
